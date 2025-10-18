d91edc2373ab11547166970a06f0c8bb2d2c31cd
FortunaD <fortunadanielmd@gmail.com>
2025-10-18 19:50:23 +0300

    Little progress on the homepage

---
+++ assets/basicLinks.php
@@ -0,0 +1,5 @@
+
+<link rel="stylesheet" href="header.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/homepage.css?v=<?php echo time(); ?>">
+    <?php include("assets/basicLinks.php")?>
+    <link rel="stylesheet" href="assets/header.css?v=<?php echo time(); ?>">
+</head>
+<body>
+    <?php include("assets/header.php") ?>    
+
+    <div class="section1">
+        <img src="assets/images/placeholder.avif" alt="" class="coverImg">
+        <div class="sec1txt1">
+            <p>STRICAM DRUMURI </p>
+        </div>
+        <div class="sec1txt2">
+            <div class="s1box">
+                <h1>20+ Ani</h1>
+                <p>De experiență în domeniu</p> 
+            </div>
+            <hr>
+            <div class="s1box">
+                <h1>20+ Ani</h1>
+                <p>De experiență în domeniu</p> 
+            </div>
+            <hr>
+            <div class="s1box">
+                <h1>20+ Ani</h1>
+                <p>De experiență în domeniu</p> 
+            </div>
+            <hr>
+            <div class="s1box">
+                <h1>20+ Ani</h1>
+                <p>De experiență în domeniu</p> 
+            </div>
+        </div>
+    </div>
+    <div class="section2">
+        <div class="title">
+            <h1>domenii <span>de activitate</span></h1>
+        </div>
+        <p class="subtitle">Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
+    </div>
+
+    <script src="assets/homepage.js"></script>
+</body>
+</html>
