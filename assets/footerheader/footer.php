aa5ddbd8229f7a4c74b2779aefdfe24ed556b65f
FortunaD <fortunadanielmd@gmail.com>
2025-10-22 10:14:32 +0300

    Alo

---
+++ assets/footerheader/footer.php
@@ -0,0 +1,23 @@
+    <div class="footer">
+        <div class="fupper">
+            <div class="fcontactform">
+                <p>Contactează-ne</p>
+                <form>
+                    <input type="text" name="nume" placeholder="Nume">
+                    <input type="text" name="prenume" placeholder="Prenume">
+                    <input type="email" name="email" placeholder="Email">
+                    <textarea placeholder="Mesajul Vostru"></textarea>
+                    <input type="submit" name="submit" value="Trimite">
+                </form>
+            </div>
+            <div class="flocatii">
+                
+            </div>
+            <div class="flocation">
+
+            </div>
+        </div>
+        <div class="flower">
+
+        </div>
+    </div>
--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="assets/homepage.css?v=<?php echo time(); ?>">
-    <?php include("assets/basicLinks.php")?>
-    <link rel="stylesheet" href="assets/header.css?v=<?php echo time(); ?>">
+    <?php include("assets/basicLinks.php") ?>
+    <link rel="stylesheet" href="assets/footerheader/footer.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="assets/footerheader/header.css?v=<?php echo time(); ?>">
 </head>
+
 <body>
-    <?php include("assets/header.php") ?>    
+    <?php include("assets/footerheader/header.php") ?>
 
     <div class="section1">
         <img src="assets/images/placeholder.avif" alt="" class="coverImg">
@@ -19,22 +22,22 @@
         <div class="sec1txt2">
             <div class="s1box">
                 <h1>20+ Ani</h1>
-                <p>De experiență în domeniu</p> 
+                <p>De experiență în domeniu</p>
             </div>
             <hr>
             <div class="s1box">
                 <h1>20+ Ani</h1>
-                <p>De experiență în domeniu</p> 
+                <p>De experiență în domeniu</p>
             </div>
             <hr>
             <div class="s1box">
                 <h1>20+ Ani</h1>
-                <p>De experiență în domeniu</p> 
+                <p>De experiență în domeniu</p>
             </div>
             <hr>
             <div class="s1box">
                 <h1>20+ Ani</h1>
-                <p>De experiență în domeniu</p> 
+                <p>De experiență în domeniu</p>
             </div>
         </div>
     </div>
@@ -44,7 +47,30 @@
         </div>
         <p class="subtitle">Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum</p>
     </div>
-
+    <div class="section3">
+        <div class="sec3right">
+            <div class="sec3rbtn active">
+                <img src="assets\images\Ellipse101.png">
+                <p>Lorem Ipsum Lorem Ipsum</p>
+            </div>
+            <div class="sec3rbtn">
+                <img src="assets\images\Ellipse100.png">
+                <p>Lorem Ipsum Lorem Ipsum</p>
+            </div>
+            <div class="sec3rbtn">
+                <img src="assets\images\Ellipse100.png">
+                <p>Lorem Ipsum Lorem Ipsum</p>
+            </div>
+            <div class="sec3rbtn">
+                <img src="assets\images\Ellipse100.png">
+                <p>Lorem Ipsum Lorem Ipsum</p>
+            </div>
+        </div>
+        <div class="sec3left">
+        </div>
+    </div>
+    <?php include("assets/footerheader/footer.php")?>
     <script src="assets/homepage.js"></script>
 </body>
+
 </html>
