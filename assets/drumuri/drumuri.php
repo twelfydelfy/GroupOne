110072840a2d6aa67d904cf88a57c0be57b43270
Flocanel <sfincafloca@gmail.com>
2025-11-05 10:16:47 +0200

    Drumuri

---
+++ assets/drumuri/drumuri.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <?php include("../basicLinks.php") ?>
+    <link rel="stylesheet" href="../footerheader/footer.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../footerheader/header.css?v=<?php echo time(); ?>">
+    <?php include("../footerheader/header.php") ?>
+    <link rel="stylesheet" href="drumuri.css">
+</head>
+
+<body>
+    <div class="center">
+        <div class="TipaBtn">
+            <h1>Inchirieaza o <span>Pula</span></h1>
+        </div>
+    </div>
+    <section class="center2">
+        <div class="Cautare">
+            <h3>Filtreazami <span>Tractorul</span></h3>
+
+
+            <h3>Tip de <span>Tractorul</span></h3>
+            <form action="">
+
+                <ul>
+                    <li>
+                        <input type="checkbox" name="checkb1" id="checkb11">
+                        <label for="checkb11">Masina</label>
+                    </li>
+                    <li>
+                        <input type="checkbox" name="checkb1" id="checkb12">
+
+                        <label for="checkb12">Tractor</label>
+                    </li>
+                </ul>
+
+
+                <h3>Tip de <span>Tractorul</span></h3>
+                <ul>
+                    <li>
+                        <input type="checkbox" name="checkb2" id="checkb21">
+
+                        <label for="checkb21">La mana doua</label>
+
+                    </li>
+                    <li>
+                        <input type="checkbox" name="checkb2" id="checkb22">
+
+                        <label for="checkb22">La mana 6</label>
+
+                    </li>
+                    <li>
+                        <input type="checkbox" name="checkb2" id="checkb23">
+
+                        <label for="checkb23">La mana 7</label>
+
+                    </li>
+                    <li>
+                        <input type="checkbox" name="checkb2" id="checkb24">
+
+                        <label for="checkb24">Nou</label>
+                    </li>
+                </ul>
+
+                <h3>Tip de <span>Tractorul</span></h3>
+                <ul>
+                    <li>
+                        <input type="checkbox" name="checkb2" id="checkb21">
+
+                        <label for="checkb21">La mana doua</label>
+
+                    </li>
+                    <li>
+                        <input type="checkbox" name="checkb2" id="checkb22">
+
+                        <label for="checkb22">Nou</label>
+                    </li>
+                </ul>
+
+
+                <input type="submit" value="Cautare">
+            </form>
+
+        </div>
+        <div class="CardBox">
+            <div class="Card">
+                <div class="CardImg">
+
+                </div>
+                <div class="CardContent">
+                    <div class="CardHeader">
+                        <h3><span>Tractor</span><br>
+                            TREX Huirex
+                        </h3>
+                    </div>
+                    <div class="CardDesc">
+                        <p>iaca asa tracotru merge</p>
+                        <p>iaca asa tractoru nu merge</p>
+                    </div>
+                    <button class="CardBuy">
+                        <h3>Buy Huiai</h3>
+                    </button>
+                </div>
+            </div>
+
+            <div class="Card">
+                <div class="CardImg">
+
+                </div>
+                <div class="CardContent">
+                    <div class="CardHeader">
+                        <h3><span>Tractor</span><br>
+                            TREX Huirex
+                        </h3>
+                    </div>
+                    <div class="CardDesc">
+                        <p>iaca asa tracotru merge</p>
+                        <p>iaca asa tractoru nu merge</p>
+                    </div>
+                    <button class="CardBuy">
+                        <h3>Buy Huiai</h3>
+                    </button>
+                </div>
+            </div>
+
+            <div class="Card">
+                <div class="CardImg">
+
+                </div>
+                <div class="CardContent">
+                    <div class="CardHeader">
+                        <h3><span>Tractor</span><br>
+                            TREX Huirex
+                        </h3>
+                    </div>
+                    <div class="CardDesc">
+                        <p>iaca asa tracotru merge</p>
+                        <p>iaca asa tractoru nu merge</p>
+                    </div>
+                    <button class="CardBuy">
+                        <h3>Buy Huiai</h3>
+                    </button>
+                </div>
+            </div>
+
+            <div class="Card">
+                <div class="CardImg">
+
+                </div>
+                <div class="CardContent">
+                    <div class="CardHeader">
+                        <h3><span>Tractor</span><br>
+                            TREX Huirex </h3>
+                    </div>
+                    <div class="CardDesc">
+                        <p>iaca asa tracotru merge</p>
+                        <p>iaca asa tractoru nu merge</p>
+                    </div>
+                    <button class="CardBuy">
+                        <h3>Buy Huiai</h3>
+                    </button>
+                </div>
+            </div>
+
+            <div class="Card">
+                <div class="CardImg">
+
+                </div>
+                <div class="CardContent">
+                    <div class="CardHeader">
+                        <h3><span>Tractor</span><br>
+                            TREX Huirex
+                        </h3>
+                    </div>
+                    <div class="CardDesc">
+                        <p>iaca asa tracotru merge</p>
+                        <p>iaca asa tractoru nu merge</p>
+                    </div>
+                    <button class="CardBuy">
+                        <h3>Buy Huiai</h3>
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+
+    <?php include("../footerheader/footer.php") ?>
+    <script src="../homepage.js?v=<?php echo time(); ?>"></script>
+    <script src="../footerheader/footer.js?v=<?php echo time(); ?>"></script>
+
+
+</body>
+
+</html>
